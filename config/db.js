0266113b2952e2ecd1e421cbc43fec1be319ce78
akshay_2304 <akshay.bhargava@jellyfishtechnologies.com>
2021-03-10 10:05:34 +0530

    Boilerplate Fixed

--- config/db.js
+++ config/db.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
-
 const { createIndexes } = require('../models/user');
-
 const db = 'mongodb+srv://krishnanshu:krish143@cluster0.wxqsk.mongodb.net/krish?retryWrites=true&w=majority';
-
 const connectDB = async () =>{
     try{
         await mongoose.connect(db , {
@@ -11,11 +8,12 @@ const connectDB = async () =>{
             useNewUrlParser: true ,
             useCreateIndex: true,
             useFindAndModify: false
-        }); //here i am connected
-        console.log('Finally i am connected with database');
+        }); 
+        console.log('Connection Established');
     }catch(err){
          console.log(err.message);
-         process.exit(1); //here i exited when find the error
+         console.log('Process Exit')
+         process.exit(1); 
     }
 }
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -8,19 +8,18 @@ app.use(bodyParser.urlencoded({ extended : true }))
 app.use(bodyParser.json())
 app.use(cors())
 
+// @Connection to database
 const connectDB = require('./config/db');
 const path = require('path');
-// Load User Routes
-const user = require('./controllers/User.js')
-
 connectDB();
 
-app.use(express.json({extended : false}));
-
+// @Controllers
+// @Load User Routes
+const user = require('./controllers/User.js')
 // Fire Up User routes
 app.use('/user', user)
 
-// ENV PORT
+// ENVIRONMENT PORT
 const PORT = process.env.PORT || 8999
 app.listen(PORT, ()=>{
     console.log(`Server Running at port ${PORT}`)
