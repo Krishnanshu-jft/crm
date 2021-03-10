ac21a7437103d968f241bba749694cab59729b26
Krishnanshu-jft <krishnanshu.mishra@jellyfishtechnologies.com>
2021-03-10 11:33:25 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { createIndexes } = require('../models/user');
-const db = 'mongodb+srv://krishnanshu:krish123@cluster0.gbrwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const db = 'mongodb+srv://krishnanshu:krishnanshu@cluster0.gbrwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const connectDB = async () =>{
     try{
         await mongoose.connect(db , {
